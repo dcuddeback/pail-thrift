57a81ff86d037c0dc210614e6ba9bcc70b62b060
David Cuddeback <david.cuddeback@gmail.com>
2013-05-31 13:51:39 -0700

    starts a new Leiningen project

---
+++ README.md
@@ -0,0 +1,13 @@
+# pail-thrift
+
+A Clojure library designed to ... well, that part is up to you.
+
+## Usage
+
+FIXME
+
+## License
+
+Copyright © 2013 David Cuddeback
+
+Distributed under the [MIT License](LICENSE).
---
+++ project.clj
@@ -0,0 +1,29 @@
+(defproject pail-thrift "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "https://github.com/dcuddeback/pail-thrift"
+  :license {:name "MIT License"
+            :url "http://opensource.org/licenses/MIT"}
+
+  :min-lein-version "2.0.0"
+
+  :dependencies [[org.clojure/clojure "1.5.1"]]
+
+  :profiles {:1.3 {:dependencies [[org.clojure/clojure "1.3.0"]]}
+             :1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
+             :1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}
+             :1.6 {:dependencies [[org.clojure/clojure "1.6.0-master-SNAPSHOT"]]}
+
+             :dev {:dependencies [[midje "1.5.1"]]
+                   :plugins [[lein-thriftc "0.1.0"]
+                             [lein-midje "3.0.1"]]
+                   :prep-tasks ["thriftc"]}}
+
+  :repositories {"sonatype" {:url "http://oss.sonatype.org/content/repositories/releases"
+                             :snapshots false
+                             :releases {:checksum :fail :update :always}}
+                 "sonatype-snapshots" {:url "http://oss.sonatype.org/content/repositories/snapshots"
+                                       :snapshots true
+                                       :releases {:checksum :fail :update :always}}}
+
+  :deploy-repositories [["releases" {:url "https://clojars.org/repo" :username :gpg :password :gpg}]
+                        ["snapshots" {:url "https://clojars.org/repo" :username :gpg :password :gpg}]])
