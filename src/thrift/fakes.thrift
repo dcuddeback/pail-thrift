8ba5d067f88b37c698d5b6faf6f5e2f360cb49b1
EricGebhart <e.a.gebhart@gmail.com>
2014-01-02 19:58:12 -0500

    added deeper thrift objects

--- src/thrift/fakes.thrift
+++ src/thrift/fakes.thrift
@@ -1,10 +1,17 @@
 namespace * pail_thrift.fakes
 
+struct Location {
+  1: optional string address;
+  2: optional string city;
+  3: optional string country;
+}
+
 union Properties {
   1: i16 age;
   2: string phone;
   3: string city;
-  }
+  4: Location location;
+}
 
 struct Name {
   1: required string firstName;
