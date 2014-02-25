56478162080c48c7d6aebac2eb2bc55b1773a31c
David Cuddeback <david.cuddeback@gmail.com>
2014-02-25 07:34:50 -0800

    lein-release plugin: preparing 0.1.1 release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject pail-thrift "0.1.1-SNAPSHOT"
+(defproject pail-thrift "0.1.1"
   :description "Serialization and partitioning strategies for using Thrift with clj-pail."
   :url "https://github.com/dcuddeback/pail-thrift"
   :license {:name "MIT License"
